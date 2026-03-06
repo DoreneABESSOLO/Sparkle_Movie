18375196c0bc75e04514c9deca7bd58731e493ec
DoreneABESSOLO <dorene.bamiyong-abessolo@laplateforme.io>
2026-03-06 10:29:11 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 from pyspark.sql import SparkSession
 from enregistrement import load_model
+import os
+
+# Désactivation complète de NativeIO Windows
+os.environ["HADOOP_HOME"] = "C:/hadoop"
+os.environ["JAVA_HOME"] = "C:/Program Files/Java/jdk-17"  # adapte si besoin
+os.environ["HADOOP_OPTIONAL_TOOLS"] = ""
+os.environ["HADOOP_ROOT_LOGGER"] = "ERROR"
+os.environ["WINUTILS"] = "false"
 
 app = FastAPI(title="Movie recommandation")
 
-spark = SparkSession.builder.appName("RecoService").getOrCreate()
+spark = (
+    SparkSession.builder
+    .appName("RecoService")
+    .config("spark.hadoop.io.nativeio.enabled", "false")
+    .config("spark.hadoop.fs.file.impl.disable.cache", "true")
+    .config("spark.driver.extraJavaOptions", "-Djava.library.path=C:/hadoop/bin")
+    .config("spark.executor.extraJavaOptions", "-Djava.library.path=C:/hadoop/bin")
+    .getOrCreate()
+)
+
 model = load_model(spark)
 
 @app.get("/recommend/{user_id}")
