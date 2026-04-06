a0b6e27b292c755fba1631853267781554206856
DoreneABESSOLO <dorene.bamiyong-abessolo@laplateforme.io>
2026-04-06 15:51:54 +0200

    Delete enregistrement.py

--- enregistrement.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def save_model(model, path="models/als_model"):
-    model.write().overwrite().save(path)
-
-
-def load_model(spark, path="models/als_model"):
-    from pyspark.ml.recommendation import ALSModel
-    return ALSModel.load(path)
